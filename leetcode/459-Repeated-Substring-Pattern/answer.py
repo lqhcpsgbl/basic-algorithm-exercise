a16baec1b5f5923d585dcfef5e83d399958cf0e2
yang.liu <yang.liu@tongdun.net>
2019-04-18 17:18:58 +0800

    add file

---
+++ leetcode/459-Repeated-Substring-Pattern/answer.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        length = len(s)
+        for i in range(length / 2 + 1):
+          sub_str = s[:i]
+          if sub_str and (length % len(sub_str) == 0) and (length / len(sub_str)) * sub_str == s:
+            return True
+        return False
